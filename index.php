42502d6cd5289fbb501cdcf502430687f9d275fb
NikolayEk <klesh85@mail.ru>
2017-05-30 19:19:32 +0300

    +sale

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>
 
 		<form name="eda" method="post" action="result.php">
-
+<!--блюдо-->
 			<div style="background: #F0F362; float: left; width: 200px; padding: 20px;">
 				<label for="food1">Блюдо1</label>
 				<input type="text" name="dish1" id="food1">
@@ -17,9 +17,23 @@
 				<input type="text" name="dish3" id="food3">
 				<label for="food4">Блюдо4</label>
 				<input type="text" name="dish4" id="food4">
-				<label for="food5">Блюдо1</label>
+				<label for="food5">Блюдо5</label>
 				<input type="text" name="dish5" id="food5">
 			</div>
+<!--количество-->
+			<div style="background: #33bdc4; float: left; width: 200px; padding: 20px;">
+				<label for="unit1">Количество</label>
+				<input type="text" name="unit1" id="unit1">
+				<label for="unit2">Количество</label>
+				<input type="text" name="unit2" id="unit2">
+				<label for="unit3">Количество</label>
+				<input type="text" name="unit3" id="unit3">
+				<label for="unit4">Количество</label>
+				<input type="text" name="unit4" id="unit4">
+				<label for="unit5">Количество</label>
+				<input type="text" name="unit5" id="unit5">
+			</div>
+<!--цена-->
 			<div style="background: #8DF362; display: inline-block; width: 200px;  padding: 20px;">
 				<label for="cost1">Цена1</label>
 				<input type="text" name="price1" id="cost1">
@@ -29,7 +43,7 @@
 				<input type="text" name="price3" id="cost3">
 				<label for="cost4">Цена4</label>
 				<input type="text" name="price4" id="cost4">
-				<label for="cost5">Цена1</label>
+				<label for="cost5">Цена5</label>
 				<input type="text" name="price5" id="cost5">
 			</div>
 			<div>
--- result.php
+++ result.php
@@ -23,14 +23,49 @@ if ($price2=='') {unset($price2);}}
 if (isset($_POST['price3'])) {$price3=$_POST['price3'];
 if ($price3=='') {unset($price3);}}
 
-if (isset($_POST['price4'])) {$price5=$_POST['price4'];
+if (isset($_POST['price4'])) {$price4=$_POST['price4'];
 if ($price4=='') {unset($price4);}}
 
 if (isset($_POST['price5'])) {$price5=$_POST['price5'];
 if ($price5=='') {unset($price5);}}
 
+if (isset($_POST['unit1'])) {$unit1=$_POST['unit1'];
+if ($unit1=='') {unset($unit1);}}
+
+if (isset($_POST['unit2'])) {$unit2=$_POST['unit2'];
+if ($unit2=='') {unset($unit2);}}
+
+if (isset($_POST['unit3'])) {$unit3=$_POST['unit3'];
+if ($unit3=='') {unset($unit3);}}
+
+if (isset($_POST['unit4'])) {$unit4=$_POST['unit4'];
+if ($unit4=='') {unset($unit4);}}
+
+if (isset($_POST['unit5'])) {$unit5=$_POST['unit5'];
+if ($unit5=='') {unset($unit5);}}
+
 if (isset($_POST['foodtime'])) {$foodtime=$_POST['foodtime'];
 if ($foodtime=='') {unset($foodtime);}}
+
+$sale;
+$summ_sale;
+$summ=$price1*$unit1+$price2*$unit2+$price3*$unit3+$price4*$unit4+$price5*$unit5;
+
+if ($summ>100 && $summ<=105) {$summ_sale=100;}
+elseif ($summ>105 && $summ<=150) {$summ_sale=($price1*$unit1+$price2*$unit2+$price3*$unit3+$price4*$unit4+$price5*$unit5)*0.95;}
+elseif ($summ>150 && $summ<=160.5) {$summ_sale=150;}
+elseif ($summ>160.5 && $summ<=200) {$summ_sale=($price1*$unit1+$price2*$unit2+$price3*$unit3+$price4*$unit4+$price5*$unit5)*0.93;}
+elseif ($summ>200 && $summ<=220) {$summ_sale=200;}
+elseif ($summ>220) {$summ_sale=($price1*$unit1+$price2*$unit2+$price3*$unit3+$price4*$unit4+$price5*$unit5)*0.9;}
+else {$summ_sale=$summ;}
+
+if ($summ>100 && $summ<=105) {$sale=$summ-100;}
+elseif ($summ>105 && $summ<=150) {$sale=($price1*$unit1+$price2*$unit2+$price3*$unit3+$price4*$unit4+$price5*$unit5)*0.05;}
+elseif ($summ>150 && $summ<=160.5) {$sale=$summ-150;}
+elseif ($summ>160.5 && $summ<=200) {$sale=($price1*$unit1+$price2*$unit2+$price3*$unit3+$price4*$unit4+$price5*$unit5)*0.07;}
+elseif ($summ>200 && $summ<=220) {$sale=$summ-200;}
+elseif ($summ>220) {$sale=($price1*$unit1+$price2*$unit2+$price3*$unit3+$price4*$unit4+$price5*$unit5)*0.1;}
+else {$sale=0;}
 
 ?>
 <!DOCTYPE html>
@@ -41,23 +76,38 @@ if ($foodtime=='') {unset($foodtime);}}
 </head>
 <body>
 	<h1>Заказано</h1>
+<!--Блюдо-->
 	<div style="background: #F0F362; float: left; width: 200px;">
+		<p><b>Блюдо</b></p>
 		<p><?=$dish1?></p>
 		<p><?=$dish2?></p>
 		<p><?=$dish3?></p>
 		<p><?=$dish4?></p>
 		<p><?=$dish5?></p>
 	</div>
+<!--количество-->
+	<div style="background: #33bdc4; float: left; width: 200px;">
+		<p><b>Количество</b></p>
+		<p><?=$unit1?></p>
+		<p><?=$unit2?></p>
+		<p><?=$unit3?></p>
+		<p><?=$unit4?></p>
+		<p><?=$unit5?></p>
+	</div>
+<!--цена-->
 	<div style="background: #8DF362; display: inline-block; width: 200px;">
+		<p><b>Цена</b></p>
 		<p><?=$price1?></p>
 		<p><?=$price2?></p>
 		<p><?=$price3?></p>
 		<p><?=$price4?></p>
 		<p><?=$price5?></p>
 	</div>
+<!--итог-->
 	<div>
-		<p><?=$foodtime?></p>
-		<p>СУММА  <?=$price1+$price2+$price3+$price4+$price5?>руб. </p>
+		<p>СУММА БЕЗ СКИДКИ <?=$summ?>руб. </p>
+		<p>ВАША СКИДКА <?=$sale?>руб. </p>
+		<p>СУММА С УЧЕТОМ СКИДКИ <?=$summ_sale?>руб. </p>
 	</div>
 </body>
 </html>
